9fed481a6181a8d3d20a7bce8b228f1e23ec798c
John(Jack) Jamison <37374979+jackjohnjamison@users.noreply.github.com>
2019-12-15 23:42:37 +0000

    Model data in emails
    Added the links model data to the email context

--- templates/render.js
+++ templates/render.js
@@ -1,8 +1,8 @@
 const Nunjucks = require('nunjucks')
 
 function getTemplate() {
-    return Nunjucks.render(`${__dirname}/mailer.njk`)
+    return Nunjucks.render(`${__dirname}/mailer.njk`, { title: 'Email title'})
 }
 
-console.log(getTemplate())
+// console.log(getTemplate())
 
